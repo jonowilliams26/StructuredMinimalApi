62998ffc78943e3712550025902fedaf7cbc89fe
jonathanjameswilliams26 <jonathanjameswilliams26@gmail.com>
2024-03-10 15:08:11 +1100

    Moved endpoint mapping to own class

--- src/ConfigureApp.cs
+++ src/ConfigureApp.cs
@@ -1,8 +1,4 @@
-﻿using Chirper.Authentication.Endpoints;
-using Chirper.Posts.Endpoints;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.OpenApi.Models;
-using Serilog;
+﻿using Serilog;
 
 namespace Chirper;
 
@@ -16,60 +12,6 @@ public static class ConfigureApp
         app.UseHttpsRedirection();
         app.MapEndpoints();
         await app.EnsureDatabaseCreated();
-    }
-
-    private static void MapEndpoints(this WebApplication app)
-    {
-        var endpoints = app.MapGroup("")
-            .WithOpenApi()
-            .AddEndpointFilter<RequestLoggingFilter>();
-
-        var publicEndpoints = endpoints.MapGroup("")
-            .AllowAnonymous();
-
-        var authorizedEndpoints = endpoints.MapGroup("")
-            .RequireAuthorization()
-            .WithAuthorizedOpenApi();
-
-        publicEndpoints.MapGroup("/auth")
-            .WithTags("Authentication")
-            .MapEndpoint<Signup>()
-            .MapEndpoint<Login>();
-
-        authorizedEndpoints.MapGroup("/posts")
-            .WithTags("Posts")
-            .MapEndpoint<GetPosts>()
-            .MapEndpoint<GetPostById>()
-            .MapEndpoint<CreatePost>()
-            .MapEndpoint<UpdatePost>()
-            .MapEndpoint<DeletePost>()
-            .MapEndpoint<LikePost>();
-    }
-
-    private static RouteGroupBuilder WithAuthorizedOpenApi(this RouteGroupBuilder builder)
-    {
-        var securityScheme = new OpenApiSecurityScheme()
-        {
-            Type = SecuritySchemeType.Http,
-            Name = JwtBearerDefaults.AuthenticationScheme,
-            Scheme = JwtBearerDefaults.AuthenticationScheme,
-            Reference = new()
-            {
-                Type = ReferenceType.SecurityScheme,
-                Id = JwtBearerDefaults.AuthenticationScheme
-            }
-        };
-
-        return builder.WithOpenApi(o => new(o)
-        {
-            Security = [new() { [securityScheme] = [] }],
-        });
-    }
-
-    private static IEndpointRouteBuilder MapEndpoint<TEndpoint>(this IEndpointRouteBuilder app) where TEndpoint : IEndpoint
-    {
-        TEndpoint.Map(app);
-        return app;
     }
 
     private static async Task EnsureDatabaseCreated(this WebApplication app)
---
+++ src/Endpoints.cs
@@ -0,0 +1,53 @@
+﻿using Chirper.Authentication.Endpoints;
+using Chirper.Posts.Endpoints;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
+namespace Chirper;
+
+public static class Endpoints
+{
+    public static void MapEndpoints(this WebApplication app)
+    {
+        var securityScheme = new OpenApiSecurityScheme()
+        {
+            Type = SecuritySchemeType.Http,
+            Name = JwtBearerDefaults.AuthenticationScheme,
+            Scheme = JwtBearerDefaults.AuthenticationScheme,
+            Reference = new()
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = JwtBearerDefaults.AuthenticationScheme
+            }
+        };
+
+        var endpoints = app.MapGroup("")
+            .AddEndpointFilter<RequestLoggingFilter>()
+            .RequireAuthorization()
+            .WithOpenApi(x => new(x)
+            {
+                Security = [new() { [securityScheme] = [] }],
+            });
+
+        endpoints.MapGroup("/auth")
+             .WithTags("Authentication")
+             .AllowAnonymous()
+             .MapEndpoint<Signup>()
+             .MapEndpoint<Login>();
+
+        endpoints.MapGroup("/posts")
+            .WithTags("Posts")
+            .MapEndpoint<GetPosts>()
+            .MapEndpoint<GetPostById>()
+            .MapEndpoint<CreatePost>()
+            .MapEndpoint<UpdatePost>()
+            .MapEndpoint<DeletePost>()
+            .MapEndpoint<LikePost>();
+    }
+
+    private static IEndpointRouteBuilder MapEndpoint<TEndpoint>(this IEndpointRouteBuilder app) where TEndpoint : IEndpoint
+    {
+        TEndpoint.Map(app);
+        return app;
+    }
+}
